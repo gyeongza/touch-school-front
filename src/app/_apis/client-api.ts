fc318b1a669998dd781384fff0d263a2237ef6c4
gyeongza <gyeongza@naver.com>
2025-02-07 14:29:01 +0900

    fix: 클라이언트 api AccessToken 오류 수정

--- src/app/_apis/client-api.ts
+++ src/app/_apis/client-api.ts
@@ -1,11 +1,22 @@
 import { saveAccessTokenInCookies } from '../_actions/cookies';
+import { ResponseInterceptor } from '../_lib/extend-fetch';
 import Fetchios from '../_lib/fetchios';
 import { AuthData } from './type';
+
+const accessTokenResponseInterceptor = createAccessTokenResponseInterceptor();
 
 export const clientApi = Fetchios.create({
   baseUrl: process.env.NEXT_PUBLIC_TSCHOOL_API_URL,
   useDefaultOptions: true,
   withCredentials: true,
+  interceptors: {
+    response: async (response, requestArgs) => {
+      let res = response;
+
+      res = await accessTokenResponseInterceptor(res, requestArgs);
+      return res;
+    },
+  },
 });
 
 export const setAuthorizationHeader = (accessToken: string) => {
@@ -16,3 +27,26 @@ export const saveAccessToken = async (token: AuthData) => {
   setAuthorizationHeader(token.accessToken);
   await saveAccessTokenInCookies(token.accessToken, token.accessTokenExpiryTime);
 };
+
+function createAccessTokenResponseInterceptor(): ResponseInterceptor {
+  return async (res, requestArgs) => {
+    try {
+      const {
+        body: { accessToken, accessTokenExpiryTime },
+      } = res.body as { body: AuthData };
+      await saveAccessToken({ accessToken, accessTokenExpiryTime });
+
+      const newRes = await clientApi.request(requestArgs[0], {
+        ...requestArgs[1],
+        headers: {
+          ...requestArgs[1]?.headers,
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
+      return newRes;
+    } catch (error) {
+      console.error(error);
+      return res;
+    }
+  };
+}
