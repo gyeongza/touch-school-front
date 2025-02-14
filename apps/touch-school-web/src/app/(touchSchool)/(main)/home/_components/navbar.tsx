5cf90b5e4ec819c5058e3b2cb44387dd33aee8d9
gyeongza <gyeongza@naver.com>
2025-02-14 17:25:23 +0900

    refactor: 준비중 dialog 제거

--- apps/touch-school-web/src/app/(touchSchool)/(main)/home/_components/navbar.tsx
+++ apps/touch-school-web/src/app/(touchSchool)/(main)/home/_components/navbar.tsx
@@ -5,38 +5,16 @@ import { AttendanceMenu } from './nav-menu/attendance-menu';
 import { NutrientMenu } from './nav-menu/nutrient-menu';
 import { SchoolInfoButton } from './nav-menu/school-info-menu';
 import { SchoolRankMenu } from './nav-menu/school-rank-menu';
-import {
-  AlertDialog,
-  AlertDialogDescription,
-  AlertDialogTitle,
-  AlertDialogHeader,
-  AlertDialogContent,
-  AlertDialogTrigger,
-  AlertDialogFooter,
-  AlertDialogCancel,
-  AlertDialogAction,
-} from '@repo/ui/alert-dialog';
+
 interface NavbarProps {
   user: User;
 }
 
 export default function Navbar({ user }: NavbarProps) {
   return (
-    <div className="flex gap-4 overflow-x-auto">
+    <div className="flex h-full gap-4 overflow-x-auto">
       <AttendanceMenu />
-      <AlertDialog>
-        <AlertDialogTrigger>
-          <NutrientMenu />
-        </AlertDialogTrigger>
-        <AlertDialogContent>
-          <AlertDialogHeader>
-            <AlertDialogDescription>준비 중인 기능이에요 🥲</AlertDialogDescription>
-          </AlertDialogHeader>
-          <AlertDialogFooter>
-            <AlertDialogAction>확인</AlertDialogAction>
-          </AlertDialogFooter>
-        </AlertDialogContent>
-      </AlertDialog>
+      <NutrientMenu />
       <SchoolRankMenu user={user} />
       <SchoolInfoButton user={user} />
     </div>
